2fc1276d0ab4e81944c62e349c0010f74fdb6e81
davidvicentealvarez <34418704+davidvicentealvarez@users.noreply.github.com>
2021-11-25 07:59:26 +0100

    update

--- GAN/model.py
+++ GAN/model.py
@@ -3,12 +3,96 @@ import torch
 import numpy as np
 
 
+class GeneratorV2(nn.Module):
+    def __init__(self, length, dictionary_size):
+        super().__init__()
+        self.dict_size = dictionary_size
+        self.length = length
+        # initial size  [1, length, dictionary_size]
+        self.reduction_layer = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=4, kernel_size=(1, dictionary_size), stride=1),
+            nn.ReLU()
+        )
+
+        self.convultion_layer = nn.Sequential(
+            nn.Conv1d(in_channels=4, out_channels=8, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(8),
+            nn.LeakyReLU(),
+            nn.Conv1d(in_channels=8, out_channels=16, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(16),
+            nn.LeakyReLU(),
+            nn.Conv1d(in_channels=16, out_channels=8, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(8),
+            nn.LeakyReLU(),
+            nn.Conv1d(in_channels=8, out_channels=4, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(4),
+            nn.LeakyReLU(),
+        )
+
+        self.deconv_layer = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=4, out_channels=1, kernel_size=(1, dictionary_size), stride=1),
+            nn.Softmax(dim=3)
+        )
+
+    def forward(self, X):
+        tensor_1d = self.reduction_layer(X).view(-1, 4, self.length)
+        new_rep = self.convultion_layer(tensor_1d).view(-1, 4, self.length, 1)
+        return self.deconv_layer(new_rep)
+
+
+class DiscriminatorV2(nn.Module):
+    def __init__(self, length, dictionary_size):
+        super().__init__()
+        self.dict_size = dictionary_size
+        self.length = length
+        # initial size  [1, length, dictionary_size]
+        self.reduction_layer = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=4, kernel_size=(1, dictionary_size), stride=1),
+            nn.ReLU()
+        )
+
+        self.convultion_layer = nn.Sequential(
+            nn.Conv1d(in_channels=4, out_channels=8, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(8),
+            nn.LeakyReLU(),
+            nn.Conv1d(in_channels=8, out_channels=16, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(16),
+            nn.LeakyReLU(),
+            nn.Conv1d(in_channels=16, out_channels=8, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(8),
+            nn.LeakyReLU(),
+            nn.Conv1d(in_channels=8, out_channels=4, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(4),
+            nn.LeakyReLU(),
+        )
+
+        self.mlp_layer = nn.Sequential(
+            nn.Linear(4 * self.length, 2048),
+            nn.LeakyReLU(),
+            nn.Linear(2048, 1024),
+            nn.LeakyReLU(),
+            nn.Linear(1024, 256),
+            nn.LeakyReLU(),
+            nn.Linear(256, 64),
+            nn.LeakyReLU(),
+            nn.Linear(64, 16),
+            nn.LeakyReLU(),
+            nn.Linear(16, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, X):
+        tensor_1d = self.reduction_layer(X).view(-1, 4, self.length)
+        new_rep = self.convultion_layer(tensor_1d).view(-1, 4 * self.length)
+        return self.mlp_layer(new_rep)
+
+
 class Generator(nn.Module):
     def __init__(self):
         super().__init__()
         self.encoder = nn.Sequential(
-            #initial size  [1, 216, 27]
-            nn.Conv2d(in_channels=1, out_channels=4 , kernel_size=(3,1), stride=1),
+            # initial size  [1, 216, 27]
+            nn.Conv2d(in_channels=1, out_channels=4, kernel_size=(3, 1), stride=1),
             # size [4, 214, 27]
             nn.BatchNorm2d(4),
             nn.LeakyReLU(),
@@ -20,11 +104,11 @@ class Generator(nn.Module):
             # size [8, 212, 23]
             nn.BatchNorm2d(8),
             nn.LeakyReLU(),
-            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=(2,1), padding="valid"),
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=(2, 1), padding="valid"),
             # size [16, 105, 21]
             nn.BatchNorm2d(16),
             nn.LeakyReLU(),
-            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(5, 3), stride=(2,1), padding="valid"),
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(5, 3), stride=(2, 1), padding="valid"),
             # size [16, 51, 19]
             nn.BatchNorm2d(16),
             nn.LeakyReLU(),
@@ -38,7 +122,8 @@ class Generator(nn.Module):
             nn.LeakyReLU()
         )
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=(5, 3), stride=(2, 1), output_padding=(1,0)),
+            nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=(5, 3), stride=(2, 1),
+                               output_padding=(1, 0)),
             # size = [32, 24, 17]
             nn.BatchNorm2d(32),
             nn.LeakyReLU(),
@@ -46,11 +131,11 @@ class Generator(nn.Module):
             # size [16, 51, 19]
             nn.BatchNorm2d(16),
             nn.LeakyReLU(),
-            nn.ConvTranspose2d(in_channels=16, out_channels=16, kernel_size=(5, 3), stride=(2,1)),
+            nn.ConvTranspose2d(in_channels=16, out_channels=16, kernel_size=(5, 3), stride=(2, 1)),
             # size [16, 105, 21]
             nn.BatchNorm2d(16),
             nn.LeakyReLU(),
-            nn.ConvTranspose2d(in_channels=16, out_channels=8, kernel_size=3, stride=(2,1), output_padding=(1,0)),
+            nn.ConvTranspose2d(in_channels=16, out_channels=8, kernel_size=3, stride=(2, 1), output_padding=(1, 0)),
             #  size [16, 105, 21]
             nn.BatchNorm2d(8),
             nn.LeakyReLU(),
@@ -71,7 +156,6 @@ class Generator(nn.Module):
     def forward(self, X):
         # return self.encoder(X)
         return self.decoder(self.encoder(X))
-
 
 
 class Discriminator(nn.Module):
@@ -127,14 +211,16 @@ class Discriminator(nn.Module):
         encoded = self.encoder(X)
         return self.mlp(encoded.view((-1, 16 * 10 * 15)))
 
+
 if __name__ == "__main__":
-    input = torch.randn(10,1,216,27)
-    g = PointGenerator()
-    output = g(input)
+    input = torch.randint(1,27,(2,216))
+    print(input.dtype)
+    emb = nn.Embedding(27, 216)
+    print(emb(input).size())
     # data = np.load("brown_corpus.npy")
     # sample = torch.tensor(data[0:128])
     # sample = sample.view(128, 1, 216, 27)
-    # sample = sample.float().to("cuda")
-    # d = Discriminator().to("cuda")
+    # sample = sample.float()
+    # d = DiscriminatorV2(216, 27)
     # output = d(sample)
-    # print(output)
+    # print(output.size())
